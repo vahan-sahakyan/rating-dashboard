64cb8d6415ae77658f15620d9c32e79be5a15e52
Vahan Sahakyan <vahan.sahakyan1996@gmail.com>
2022-10-11 19:22:07 +0400

    dynamic source.src for disabling cache

---
+++ links.js
@@ -0,0 +1,4 @@
+const LINK_1 =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vRaASzI_auqFTp1CdHyOe23fyvSvYoU8GJFRYybTJV898C4DYFkif5-HD4MXLPpuFR3PRHx24d2jU8-/pubhtml?gid=0&single=true';
+const LINK_2 =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4S0lgFSyweb_cXnVP-Y3mPHbizTaEfmHCRXf8EJkXXoSok77vofwdPIlh4Lxslvu5n_wN3uVm3-cc/pubhtml?gid=0&single=true';
--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const iframe = document.querySelector('iframe');
 
-const LINK_1 =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vRaASzI_auqFTp1CdHyOe23fyvSvYoU8GJFRYybTJV898C4DYFkif5-HD4MXLPpuFR3PRHx24d2jU8-/pubhtml?gid=0&single=true';
-const LINK_2 =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4S0lgFSyweb_cXnVP-Y3mPHbizTaEfmHCRXf8EJkXXoSok77vofwdPIlh4Lxslvu5n_wN3uVm3-cc/pubhtml?gid=0&single=true';
-
+// timer
 const timer = window?.location?.search?.split('=')[1];
 console.log({ timer });
+const isTimerProblem = timer === undefined || timer < 2000;
+const SECONDS = isTimerProblem ? 7000 : timer;
 
 iframe.src = LINK_1;
 let current = false;
-
-const isTimerProblem = timer === undefined || timer < 2000;
-
-const SECONDS = isTimerProblem ? 7000 : timer;
 console.log({ SECONDS });
 
+//////////////// main
 setInterval(() => {
+  updateScriptSrcRandomly();
+  swapFrames();
+}, SECONDS);
+
+/////////////////////////////////////
+/////////////////////////// FUNCTIONS
+
+function swapFrames() {
   iframe.src = current ? LINK_1 : LINK_2;
   current = !current;
-}, SECONDS);
+}
+
+function updateScriptSrcRandomly() {
+  const rand = Math.floor(Math.random() * 999999999999999);
+  document.querySelector('#main').src = `./script.js?${rand}`;
+}
